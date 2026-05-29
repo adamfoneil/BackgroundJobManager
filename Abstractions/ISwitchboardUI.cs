cb1c9999b120ed5ee92dd014bdb7ab413ac2d9ba
adamfoneil <adamfoneil@proton.me>
2026-05-29 14:57:29 -0400

    refinements to switchboard UI interface

--- Abstractions/ISwitchboardUI.cs
+++ Abstractions/ISwitchboardUI.cs
@@ -6,4 +6,17 @@
 public interface ISwitchboardUI
 {
     Task<IDictionary<string, (NextRunInfo? NextRun, LastRunInfo? LastRun)>> GetAdminViewAsync();
+    /// <summary>
+    /// mark service as Scheduled
+    /// </summary>
+    Task EnableAsync(string serviceType);
+    /// <summary>
+    /// mark service as Disabled
+    /// </summary>
+    Task DisableAsync(string serviceType);
+    /// <summary>
+    /// run service immediately (set NextRun to now). Note that if the service is already running, this will not do anything until the current run 
+    /// is finished and the next run is scheduled. Use with caution to avoid multiple instances of the same service running at the same time.
+    /// </summary>
+    Task RunNowAsync(string serviceType);
 }
